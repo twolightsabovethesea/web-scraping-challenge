9574b0d3b0a14eaf4f26aef5c5ec422025d3827a
Meredith Hopkins <twolightsabovethesea@gmail.com>
2021-09-09 13:40:33 -0400

    worked on app.py

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+
+# import pymongo and scrape_mars
+import pymongo
+import scrape_mars
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = client.mars_db
+
+# Drops collection if available to remove duplicates
+db.mars.drop()
+
+ #create route that renders index.html template
+@app.route("/")
+def index():
+    player_dictionary = {"player_1": "Jessica",
+                         "player_2": "Mark"}
+    return render_template("index.html", dict=player_dictionary)
+
+
+# Creates a collection in the database and inserts two documents
+db.mars.insert_many(
+    [
+        {
+            'player': 'Jessica',
+            'position': 'Point Guard'
+        },
+        {
+            'player': 'Mark',
+            'position': 'Center'
+        }
+    ]
+)
+
+
+# Set route
+@app.route('/')
+def index():
+    # Store the entire team collection in a list
+    teams = list(db.team.find())
+    print(teams)
+
+    # Return the template with the teams list passed in
+    return render_template('index.html', teams=teams)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
